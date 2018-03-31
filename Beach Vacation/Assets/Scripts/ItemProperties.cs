7f66bb48c846508b4d887f7a2032c2eff46d8dfe
robhaywood12 <robhaywood12@gmail.com>
2018-03-30 22:21:15 -0500

    eating & drinking items
    finished part 6

---
+++ Beach Vacation/Assets/Scripts/ItemProperties.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemProperties : MonoBehaviour {
+
+    [Header("Your Consumables")]
+    public string itemName;
+
+    [SerializeField] private bool food;
+    [SerializeField] private bool water;
+    [SerializeField] private bool health;
+    [SerializeField] private float value;
+
+
+    [SerializeField] private PlayerVitals playerVitals;
+
+    public void Interaction()
+    {
+        if (food)
+        {
+            playerVitals.hungerSlider.value += value;
+        }
+
+        else if (water)
+        {
+            playerVitals.thirstSlider.value += value;
+        }
+
+        else if (health)
+        {
+            playerVitals.healthSlider.value += value;
+        }
+    }
+}
---
+++ Beach Vacation/Assets/Scripts/RaycastManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastManager : MonoBehaviour
+{
+    private GameObject raycastedObj;
+
+    [Header("Raycast Settings")]
+    [SerializeField] private float rayLength = 10;
+    [SerializeField] private LayerMask newLayerMask;
+
+    [Header("References")]
+    [SerializeField] private Image crossHair;
+    [SerializeField] private Text itemNameText;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, newLayerMask.value))
+        {
+            if (hit.collider.CompareTag("Consumable"))
+            {
+                CrosshairActive();
+                raycastedObj = hit.collider.gameObject;
+                // update UI name
+                itemNameText.text = raycastedObj.GetComponent<ItemProperties>().itemName;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    //Object properties
+                    raycastedObj.GetComponent<ItemProperties>().Interaction();
+                    raycastedObj.SetActive(false);
+                }
+            }
+        }
+
+        else
+        {
+            CrosshairNormal();
+            // item name back to normal
+            itemNameText.text = null;
+        }
+    }
+
+    void CrosshairActive()
+    {
+        crossHair.color = Color.red;
+    }
+
+    void CrosshairNormal()
+    {
+        crossHair.color = Color.white;
+    }
+
+
+}
