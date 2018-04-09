671a17d119030093dc1fa141a9559c69092842ab
Herctic <lin.alex@utexas.edu>
2018-04-08 19:09:35 -0500

    LIKE NOTHING OF THE INVENTORY SYSTEM BUT ITS THERE I GUESS

---
+++ Beach Vacation/Assets/inventoryUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class inventoryUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Inventory.instance;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
